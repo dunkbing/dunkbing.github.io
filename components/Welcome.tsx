0c3508cdb09a12661b1bc5ba2751bfeb6c7830df
binh.buidang <dangbinh4869@gmail.com>
2021-08-19 22:57:20 +0700

    add: welcom

---
+++ components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Welcome: React.FC<{message: string}> = ({message}) => {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      verticalAlign: 'center',
+    }}>
+      <h1 className="title m-0.5">{message}</h1>
+    </div>
+  );
+};
+
+export default Welcome;
