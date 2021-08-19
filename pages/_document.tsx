5b5185e693d36a516d0e16980c149bb2ae5e9593
binh.buidang <dangbinh4869@gmail.com>
2021-08-20 02:25:21 +0700

    add: google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link id="favicon" rel="shortcut icon" href="/favicon.ico" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-201839371-1" />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-201839371-1');`
+          }}/>
         </Head>
         <body className="bg-white dark:bg-black text-gray-900 dark:text-white">
           <Main />
