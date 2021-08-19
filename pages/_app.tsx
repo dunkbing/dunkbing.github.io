8dfdaad4ad91751323f285216ac645f6080fb77a
binh.buidang <dangbinh4869@gmail.com>
2021-08-20 02:25:21 +0700

    add: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    if (process.browser) {
+      (window as any).gtag('config', 'UA-201839371-1', {
+        page_path: url,
+      });
+    }
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       <Component {...pageProps} />
