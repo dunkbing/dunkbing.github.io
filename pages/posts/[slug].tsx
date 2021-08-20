efb5be8e9a4c2f573f3ab0126bfb034901faa769
binh.buidang <dangbinh4869@gmail.com>
2021-08-20 14:34:30 +0700

    update css.

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -51,7 +51,7 @@ const PostPage = ({ source, frontMatter }: PostPageProps): JSX.Element => {
         <p className="mb-10 text-sm text-gray-500 dark:text-gray-400">
           {format(parseISO(frontMatter.date), 'MMMM dd, yyyy')}
         </p>
-        <div className="note-content prose dark:prose-dark">
+        <div className="prose dark:prose-dark">
           <MDXRemote {...source} components={components} />
         </div>
       </article>
