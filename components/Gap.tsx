8bc85405e373feed0ed0a8d0400ad40b213d02a3
binh.buidang <dangbinh4869@gmail.com>
2022-03-23 00:00:41 +0700

    add: gap.

---
+++ components/Gap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gap = (): JSX.Element => <div style={{ width: 20, height: 20 }} />;
+
+Gap.XS = () => <div style={{ width: 10, height: 10 }} />;
+
+Gap.S = () => <div style={{ width: 20, height: 20 }} />;
+
+Gap.M = () => <div style={{ width: 40, height: 40 }} />;
+
+Gap.L = () => <div style={{ width: 60, height: 60 }} />;
+
+export default Gap;
