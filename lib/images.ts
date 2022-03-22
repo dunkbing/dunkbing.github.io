39aee5c6ceb11afbcaa0e262097018bcde36f617
binh.buidang <dangbinh4869@gmail.com>
2022-03-23 00:00:26 +0700

    add: images.

---
+++ lib/images.ts
@@ -0,0 +1,6 @@
+import cyberPurr from '../public/images/projects/cyber-purr/1.png';
+import bonkingBall from '../public/images/projects/jumping-ball/1.png';
+import floppyShip from '../public/images/projects/floppy-ship/1.webp';
+import defaultImg from '../public/images/default.jpg';
+
+export { cyberPurr, bonkingBall, floppyShip, defaultImg };
