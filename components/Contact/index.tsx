1fbe523e43318abd07380256c27c21190500cb80
Bùi Đặng Bình <dangbinh4869@gmail.com>
2022-10-12 14:18:11 +0700

    remove resume.

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Resume from './Resume';
 import Social from './Social';
 import TimeLine from './TimeLine';
 
@@ -14,7 +13,6 @@ const Contact: React.FC = () => {
       }}
     >
       <TimeLine />
-      <Resume />
       <Social />
     </div>
   );
