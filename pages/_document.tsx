5926bfe657bea9fbae089444bac82aaad85958e4
binh.buidang <dangbinh4869@gmail.com>
2024-01-15 18:35:00 +0700

    Remove Google Analytics code and configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,11 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-import { GA_ID } from '@lib/ga';
-
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
           <link id="favicon" rel="shortcut icon" href="/favicon.ico" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${GA_ID}');`,
-            }}
-          />
           <script
             async
             src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
