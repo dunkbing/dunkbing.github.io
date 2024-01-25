034aa58da2ee652c686fe46e6a240fa036fca2a3
binh.buidang <dangbinh4869@gmail.com>
2024-01-25 19:37:01 +0700

    add facebook badge.

--- components/Contact/Social.tsx
+++ components/Contact/Social.tsx
@@ -1,39 +1,39 @@
 import Link from 'next/link';
 import React from 'react';
-import { FiTwitter, FiInstagram, FiGithub } from 'react-icons/fi';
+import { FiTwitter, FiInstagram, FiFacebook, FiGithub } from 'react-icons/fi';
 
 const Social: React.FC = () => {
   return (
-    <div className="font-mono flex flex-row items-center justify-center my-2">
-      Find me on
+    <div className="font-mono flex flex-row space-x-2 items-center justify-center my-2">
+      <span>Find me on</span>
       <Link
         href="https://twitter.com/dunkbingg"
         target="_blank"
         aria-label="Twitter"
       >
-        <FiTwitter size={24} style={{ marginLeft: 10, marginTop: 4 }} />
+        <FiTwitter size={24} />
+      </Link>
+      <Link
+        href="https://www.facebook.com/bingdevtrai/"
+        target="_blank"
+        aria-label="Facebook"
+      >
+        <FiFacebook size={24} />
       </Link>
       <Link
         href="https://www.instagram.com/dunkbingg/"
         target="_blank"
         aria-label="Instagram"
       >
-        <FiInstagram size={24} style={{ marginLeft: 10, marginTop: 4 }} />
+        <FiInstagram size={24} />
       </Link>
       <Link
         href="https://github.com/dunkbing"
         target="_blank"
         aria-label="Github"
       >
-        <FiGithub size={24} style={{ marginLeft: 10, marginTop: 4 }} />
+        <FiGithub size={24} />
       </Link>
-      {/* <Link
-        href="https://www.facebook.com/dunkbinggg/"
-        target="_blank"
-        aria-label="Facebook"
-      >
-        <FiFacebook size={24} style={{ marginLeft: 10, marginTop: 4 }} />
-      </Link> */}
     </div>
   );
 };
